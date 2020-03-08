f1b2e180d1eb3f61c8afc27533a176d3f40fb911
Kyle Kneitinger <kyle@kneit.in>
2020-03-08 16:10:33 -0700

    Add PageType and Unit support to mod page

--- src/page.rs
+++ src/page.rs
@@ -1,85 +1,58 @@
-use crate::geometry::PageSpace;
-use crate::geometry::Pathable;
+use crate::geometry::{PageSpace, Pathable};
+use crate::units::*;
 use euclid::Size2D;
-use std::fmt;
 use svg::node::element::Text;
+use svg::node::Node;
 use svg::node::Text as PrimitiveText;
 use svg::Document;
-
-pub enum Unit {
-    Px,
-    Mm,
-    In,
-}
-
-pub const DPI: f64 = 96.;
-pub const INCH: f64 = DPI;
-pub const PX: f64 = 1.;
-pub const MM: f64 = DPI / 25.4; // 1in == 25.4mm
-pub const CM: f64 = MM * 10.;
-pub const PT: f64 = INCH / 72.;
-
-impl Unit {
-    pub fn to_string_with_val(&self, n: f64) -> String {
-        format!("{}{}", n, self)
-    }
-
-    pub fn scale(&self) -> f64 {
-        match self {
-            Self::Px => 1.,
-            Self::Mm => 3.77953,
-            Self::In => 96.,
-        }
-    }
-}
-
-impl fmt::Display for Unit {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Self::Px => "px",
-                Self::Mm => "mm",
-                Self::In => "in",
-            }
-        )
-    }
-}
 
 pub enum PageType {
     BlackPad,
     A4,
     A6,
+    Pad11x14,
     Other(f64, f64, Unit),
 }
 
 impl PageType {
     pub fn dimensions(&self) -> Size2D<f64, PageSpace> {
         match self {
-            Self::BlackPad => Size2D::new(20.7 * CM, 29.35 * CM),
-            Self::A6 => Size2D::new(105. * MM, 148. * MM),
-            Self::A4 => Size2D::new(210. * MM, 297. * MM),
-            Self::Other(w, h, _) => Size2D::new(w * MM, h * MM),
+            Self::BlackPad => Size2D::new(20.7, 29.35),  // cm
+            Self::A6 => Size2D::new(105., 148.),         // mm
+            Self::A4 => Size2D::new(210., 297.),         // mm
+            Self::Pad11x14 => Size2D::new(11., 14.),     // in
+            Self::Other(w, h, _) => Size2D::new(*w, *h), // other
+        }
+    }
+
+    pub fn unit(&self) -> Unit {
+        match self {
+            Self::BlackPad => Unit::Cm,
+            Self::A6 => Unit::Cm,
+            Self::A4 => Unit::Cm,
+            Self::Pad11x14 => Unit::In,
+            Self::Other(_, _, u) => *u,
         }
     }
 }
 
 pub struct Page {
     doc: Document,
-    //width: f64,
-    //height: f64,
-    //unit: Unit,
+    /* For future use
+    _width: f64,
+    _height: f64,
+    _unit: Unit,
+    */
 }
 
 impl Page {
     pub fn add<T: Pathable>(&mut self, p: &T) {
-        self.doc = self.doc.clone().add(p.to_path());
+        self.doc.append(p.to_path());
     }
 
     pub fn add_comment<T: Into<String>>(&mut self, content: T) {
         let text = Text::new().add(PrimitiveText::new(content));
-        self.doc = self.doc.clone().add(text);
+        self.doc.append(text);
     }
 
     pub fn new(width: f64, height: f64, unit: Unit) -> Page {
@@ -87,13 +60,15 @@ impl Page {
             doc: Document::new()
                 .set("width", unit.to_string_with_val(width))
                 .set("height", unit.to_string_with_val(height)),
-            //width,
-            //height,
-            //unit,
         }
     }
 
+    pub fn new_from_pagetype(pagetype: PageType) -> Page {
+        let dimensions = pagetype.dimensions();
+        Page::new(dimensions.width, dimensions.height, pagetype.unit())
+    }
+
     pub fn save<T: AsRef<std::path::Path>>(&self, filepath: T) {
-        svg::save(filepath, &self.doc).unwrap()
+        svg::save(filepath, &self.doc).expect("sdfds");
     }
 }
