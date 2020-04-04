6e934d9ebca1c150a321fe15523b52deb7fef5a8
Kyle Kneitinger <kyle@kneit.in>
2020-04-04 14:59:25 -0700

    Use group in basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -7,9 +7,12 @@ fn main() {
     let poly = Poly::new(vec![point(1, 1), point(9, 1), point(9, 9), point(1, 9)]);
     let lines = poly.hatch(0.01, 0., 0.);
 
+    let mut group = Group::new("test_group");
+
     for l in lines {
-        page.add(&l);
+        group.add(&l);
     }
 
+    page.add_group(group);
     page.save("image.svg".to_string());
 }
