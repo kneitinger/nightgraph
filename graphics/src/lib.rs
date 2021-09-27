b263ec7861d50eba1ed89fd4c228591d261946a7
Kyle Kneitinger <kyle@kneit.in>
2021-09-26 21:48:05 -0700

    Remove geometry_3d module until rewrite

--- graphics/src/lib.rs
+++ graphics/src/lib.rs
@@ -1,8 +1,7 @@
 //#![warn(clippy::all, clippy::pedantic, clippy::cargo)]
 
-pub mod geometry;
-//pub mod geometry_3d;
 pub mod canvas;
+pub mod geometry;
 pub mod prelude;
 pub mod render;
 pub mod units;
