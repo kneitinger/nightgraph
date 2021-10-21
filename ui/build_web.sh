20ce56596e737c392cd66253390126679816410c
Kyle Kneitinger <kyle@kneit.in>
2021-10-21 11:55:29 -0700

    Specify cargo metadata format to prevent warning

--- ui/build_web.sh
+++ ui/build_web.sh
@@ -2,8 +2,8 @@
 set -eu
 
 # ./setup_web.sh # <- call this first!
-TARGET_DIR="$(cargo metadata  | jq -r  '.target_directory')"
-CRATE_NAME="$(cargo metadata  | jq -r .resolve.root | cut -d' ' -f1)"
+TARGET_DIR="$(cargo metadata --format-version=1 | jq -r  '.target_directory')"
+CRATE_NAME="$(cargo metadata --format-version=1 | jq -r .resolve.root | cut -d' ' -f1)"
 CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}" # for those who name crates with-kebab-case
 
 # This is required to enable the web_sys clipboard API which egui_web uses
